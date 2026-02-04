7be8580b3a63e32220f50dc38496d252ff448fe5
ozpool <jittendersingh389@gmail.com>
2026-02-04 19:05:40 +0530

    feat: implement deposit logic with validation

--- contracts/core/LendingPool.sol
+++ contracts/core/LendingPool.sol
@@ -11,4 +11,25 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
 
     event Deposit(address indexed user, address indexed asset, uint256 amount);
     event Withdraw(address indexed user, address indexed asset, uint256 amount);
+
+    /**
+     * @notice Deposit ERC20 tokens into the lending pool
+     * @param asset The ERC20 token address
+     * @param amount The amount to deposit
+     */
+    function deposit(address asset, uint256 amount)
+        external
+        nonReentrant
+    {
+        require(asset != address(0), "INVALID_ASSET");
+        require(amount > 0, "INVALID_AMOUNT");
+
+        // Effects
+        balances[msg.sender][asset] += amount;
+
+        // Interactions
+        IERC20(asset).transferFrom(msg.sender, address(this), amount);
+
+        emit Deposit(msg.sender, asset, amount);
+    }
 }
