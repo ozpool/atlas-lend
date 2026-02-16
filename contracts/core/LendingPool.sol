c53e696b951c667438bc7954e70ad743eececb89
ozpool <jittendersingh389@gmail.com>
2026-02-16 18:57:39 +0530

    feat: wire interest rate model into LendingPool

--- contracts/core/LendingPool.sol
+++ contracts/core/LendingPool.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "../interfaces/ILendingPool.sol";
+import "../interfaces/IInterestRateModel.sol";
 
 contract LendingPool is ILendingPool, ReentrancyGuard {
     /// @dev Loan-to-Value ratio (75%)
@@ -12,6 +14,8 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
 
     uint256 public totalDeposits;
     uint256 public totalBorrows;
+
+    IInterestRateModel public interestRateModel;
 
     /// @dev user => asset => deposited amount
     mapping(address => mapping(address => uint256)) internal balances;
@@ -23,6 +27,11 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
     event Withdraw(address indexed user, address indexed asset, uint256 amount);
     event Borrow(address indexed user, address indexed asset, uint256 amount);
     event Repay(address indexed user, address indexed asset, uint256 amount);
+
+    constructor(address _rateModel) {
+        require(_rateModel != address(0), "INVALID_RATE_MODEL");
+        interestRateModel = IInterestRateModel(_rateModel);
+    }
 
     // -------------------------------------------------------
     // DEPOSIT
@@ -36,10 +45,7 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
 
         IERC20(asset).transferFrom(msg.sender, address(this), amount);
 
-        // User accounting
         balances[msg.sender][asset] += amount;
-
-        // Protocol accounting
         totalDeposits += amount;
 
         emit Deposit(msg.sender, asset, amount);
@@ -56,13 +62,9 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
         require(amount > 0, "INVALID_AMOUNT");
         require(balances[msg.sender][asset] >= amount, "INSUFFICIENT_BALANCE");
 
-        // Check health after withdrawal
         require(_isSolvent(msg.sender, asset, amount, 0), "WOULD_BECOME_UNDERCOLLATERALIZED");
 
-        // User accounting
         balances[msg.sender][asset] -= amount;
-
-        // Protocol accounting
         totalDeposits -= amount;
 
         IERC20(asset).transfer(msg.sender, amount);
@@ -80,13 +82,9 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
     {
         require(amount > 0, "INVALID_AMOUNT");
 
-        // Check LTV
         require(_isSolvent(msg.sender, asset, 0, amount), "INSUFFICIENT_COLLATERAL");
 
-        // User accounting
         debts[msg.sender][asset] += amount;
-
-        // Protocol accounting
         totalBorrows += amount;
 
         IERC20(asset).transfer(msg.sender, amount);
@@ -125,10 +123,7 @@ contract LendingPool is ILendingPool, ReentrancyGuard {
 
         uint256 repayAmount = amount > debt ? debt : amount;
 
-        // User accounting
         debts[user][asset] -= repayAmount;
-
-        // Protocol accounting
         totalBorrows -= repayAmount;
 
         return repayAmount;
